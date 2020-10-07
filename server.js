6fdc7b9c5061007434ad3659c295e147bc799b48
Joshwa Facistol <fastline2123@gmail.com>
2020-10-07 12:19:56 +0800

    Update Production

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.use(express.json({ extended: true }));
 // Routes
 app.use('/api', require('./routes/index'));
 
+// Production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
+
 // Port
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port:${port}`));
